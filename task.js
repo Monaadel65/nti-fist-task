68e8a6e59bd81768f70abfe0141a9138d59340cf
Monaadel65 <104681621+Monaadel65@users.noreply.github.com>
2022-05-17 11:10:59 +0200

    Add files via upload

---
+++ task.js
@@ -0,0 +1,91 @@
+const fs= require('fs');
+
+const addSutdent=(id,Sname,grades,comment,total)=>{
+    const allStudents=loadStudents();
+    total=0;
+    const dublicatedId=allStudents.filter((s)=>{
+        return s.id===id
+    });
+    grades.forEach(element => {
+        return total+=element;
+    });
+    if(dublicatedId.length===0){
+        allStudents.push({
+            id,
+            Sname,
+            grades,
+            comment,
+            total,
+        });
+        saveStudent(allStudents);
+        console.log('success');
+    }
+    else{
+        console.log('Error');
+    }
+}
+
+
+
+//load all students data from student.json
+
+const loadStudents=()=>{
+    try{
+        const studentsBuuffer= fs.readFileSync('student.json').toString();
+        return JSON.parse(studentsBuuffer);
+    }
+    catch(e){
+        return [];
+    }
+}
+
+//save student data to student.json file
+
+const saveStudent=(studentData)=>{
+    const saveData=JSON.stringify(studentData);
+    fs.writeFileSync('student.json',saveData);
+}
+
+//delete student by id
+const deleteStudent=(id)=>{
+    const allStudents=loadStudents();
+    const keepStudent=  allStudents.filter((ele)=>{
+        return ele.id !==id;
+    });
+    saveStudent(keepStudent);
+    console.log('deleted');
+}
+
+//read student data 
+const readStudent=(id)=>{
+    const allStudents=loadStudents();
+    const studentData=allStudents.find(ele=>ele.id===id);
+    if(studentData){
+    console.log(studentData.Sname,studentData.total);
+    }
+    else{
+        console.log('not found')
+    }
+}
+
+//list all students
+
+const listStudents=()=>{
+    const allStudents=loadStudents();
+    const list=allStudents.forEach(element => {
+        console.log(element.Sname)
+        return element;
+        
+    });
+}
+
+
+
+
+
+module.exports={
+    addSutdent,
+    deleteStudent,
+    readStudent,
+    listStudents,
+}
